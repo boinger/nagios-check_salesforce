14f95fd79208611e7b86b4724c980491d27a5156
jv <jeff@jeffvier.com>
2020-01-28 11:05:07 -0800

    --

--- test.spool.sh
+++
@@ -1,2 +0,0 @@
-for X in `ls ~/.sfdx | grep json | grep -v key.json`; do ; done
-./check_salesforce-soql.sh -u admin@icix.wawainc01 -Q SELECT Id, Name, icix_v1__Request__r.Name, icix_v1__Status__c FROM ICIX_V1__Requeue_Message__c WHERE icix_v1__Status__c = 'Open' ORDER BY CreatedDate ASC --nolimit -c 1
